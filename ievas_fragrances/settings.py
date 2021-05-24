67f1872ca174ddb51ed0e946e0cad39f33726a77
Leefarmer83 <leefarmer83@gmail.com>
2021-05-24 13:30:41 +0000

    change there to get the SECRET_KEY from the environment as well as adding logic to set the debug value (lines 27 and 30). I've also fixed an indentation error on line 17.

--- ievas_fragrances/settings.py
+++ ievas_fragrances/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 import dj_database_url
 if os.path.exists("env.py"):
-  import env
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+r=w@_&8i^426idnz^vu4+xw^6_ozi!5%&d^e=)*-)t=5vrm7*'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ['ievas-fragrances.herokuapp.com', 'localhost']
 
